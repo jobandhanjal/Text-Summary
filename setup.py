d91fa29dc02f35d0201d5e66bfa229195729a1e6
Jobanjit Singh <132876498+jobandhanjal@users.noreply.github.com>
2025-01-24 19:49:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summary"
-AUTHOR_USER_NAME = "Jobanjit Singh"
+AUTHOR_USER_NAME = "jobandhanjal"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "dhanjaljobanjit@gmail.com"
 
